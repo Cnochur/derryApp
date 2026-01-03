d6af4f6bd3ef34a47520554e6334a80ed2adc19e
cnochur <cnochursaunders@gmail.com>
2026-01-03 13:13:56 +0000

    View now gets bus stop data

--- buschecker/views.py
+++ buschecker/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from buschecker.services import getPossibleStops
 # from .models import
 # Create your views here.
 
 def home(request):
+    if request.method == 'POST':
+        directionChoice: str = request.POST['directionChoice']
+        areaChoice: str = request.POST['areaChoice'].title()
+
+        stops = getPossibleStops(directionChoice, areaChoice)
+
+        print(stops)
     return render(request, template_name='buschecker/index.html')
