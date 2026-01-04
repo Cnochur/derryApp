f2aa053d29e331de923cba0b60336d26ffbab419
cnochur <cnochursaunders@gmail.com>
2026-01-04 11:03:50 +0000

    removed redundant view

--- buschecker/views.py
+++ buschecker/views.py
@@ -14,16 +14,3 @@ def home(request):
     return render(request, template_name='buschecker/index.html')
 
 
-def stops(request):
-    area = request.GET['areaChoice']
-    direction = request.GET['directionChoice']
-    areaStops = getPossibleStops(direction, area)
-
-    stopsList = []
-
-    for stop in areaStops:
-        stopsList.append(
-            {"id": stop.id, "name": stop.name, "latitude": stop.latitude, "longitude": stop.longitude}
-        )
-
-    return JsonResponse(stopsList, safe=False)
